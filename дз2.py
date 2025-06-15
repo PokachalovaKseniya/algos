044862bd9f5ddec36bfdaa0596019d78e60fa8a7
PokachalovaKseniya <ksupro228@mail.ru>
2025-06-15 14:36:46 +0300

    .

--- дз2.py
+++ дз2.py
@@ -1,10 +1,9 @@
-# coding=windows-1251 
-import unittest
+п»ї# cooding=windows-1251 
 import numpy as np
 
 def simpson_rule(f, a, b, n):
     if n % 2 == 1:
-        n += 1  # Делаем n четным
+        n += 1  
     h = (b - a) / n
     x = np.linspace(a, b, n+1)
     fx = f(x)
@@ -12,22 +11,9 @@ def simpson_rule(f, a, b, n):
     integral = (h / 3) * (fx[0] + 4 * sum(fx[1:-1:2]) + 2 * sum(fx[2:-2:2]) + fx[-1])
     return integral
 
-
-f1 = lambda x: (3 - x) - (-3 + x) 
-f2 = lambda x: (3 + x) - (-3 - x) 
-
-def func(n):
-    result1 =simpson_rule(f1, 0, 3, n)
-    result2=simpson_rule(f2, -3, 0, n)
-    return result1 + result2
-
-for n in range(1,10000,1000):
-    print(f"результат для n = {n}: " + str(func(n)))
-
-class TestinFunc(unittest.TestCase):
-    def test_func(self):
-        self.assertAlmostEqual(func(100), func(1000), places=7)
-        self.assertAlmostEqual(func(1000), func(5000), places=7)
-        self.assertAlmostEqual(func(5000), func(10000), places=7)
-if __name__ == "__main__":
-    unittest.main()
+f1 = lambda x: 6 + 2 * x
+f2 = lambda x: 6 - 2 * x
+res1 = simpson_rule(f1, -3, 0, 1)
+res2 = simpson_rule(f2, 0, 3, 1)
+area = res1 + res2
+print(area)
